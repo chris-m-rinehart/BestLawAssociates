c444aa2cac0d9819ffaecd7877eafab3b6b79509
Chris Rinehart <chris.m.rinehart@gmail.com>
2024-02-16 16:56:56 -0500

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,34 @@
 import $ from 'jquery'
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a [href*=' + id + ']').classList
+          .add('active');
+        });
+      };
+    });
+};
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("logo").style.height = "30px";
+  } else {
+    document.getElementById("logo").style.height = "90px";
+  }
+}
 
 function redirectToEOIR() {
     window.location.href = 'https://acis.eoir.justice.gov/en/';
-}
-
-const nav = document.querySelector(".nav");
-window.addEventListener("scroll", fixNav);
-
-function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 100) {
-    nav.classList.add("active");
-  } else {
-    nav.classList.remove("active");
-  }
 }
