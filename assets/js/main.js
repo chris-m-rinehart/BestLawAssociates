e83a457612a2a36f4b7dcede63c215fbe220dce6
Chris Rinehart <chris.m.rinehart@gmail.com>
2024-02-16 14:16:11 -0500

    Drastically went backwards through the style.css and index.html files to make it more modifiable and viable going forward.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ import $ from 'jquery'
 function redirectToEOIR() {
     window.location.href = 'https://acis.eoir.justice.gov/en/';
 }
+
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
