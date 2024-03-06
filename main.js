55a59c780063daaf18847fd98ed6713332e26221
Chris Rinehart <chris.m.rinehart@gmail.com>
2024-03-06 14:18:21 -0500

    Removed php files, made changes to html and css started work on external pages

---
+++ main.js
@@ -0,0 +1,60 @@
+import $ from 'jquery'
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav');
+alert();
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 80;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('current');
+          document.querySelector('header nav a [href*=' + id + ']').classList.add('current');
+        });
+      };
+    });
+};
+
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBlqLS-OwTXaorGhskuTv10a7vEtcmwHB8&callback=initMap';
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 39.962408, lng: -75.153338 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "BestAndAssociates",
+  });
+}
+
+initMap();
